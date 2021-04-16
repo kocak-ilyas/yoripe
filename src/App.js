4ef4e55fa579cfb596e5a31bc253c7f9fdb80209
ilyas kocak <kocakilyas18@gmail.com>
2021-04-16 11:35:20 +0300

    apiary done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useState } from 'react';
+import API from 'apisauce';
 import './App.css';
 
+const baseURL = 'https://private-e76dd9-kocakilyas.apiary-mock.com';
+const ApiCall = API.create({ baseURL: baseURL });
 function App() {
-  return (
-    <div className="App">
-     
-          Learn React
-       
-    </div>
-  );
+	const [ data, setData ] = useState(null);
+
+	const handleGet = () => {
+		ApiCall.get('/api')
+			.then((Response) => {
+				setData(Response.data.message);
+				console.log('GET Method is success');
+				console.log(data);
+			})
+			.catch((error) => {
+				console.log('error :>> ', error);
+			});
+	};
+	const handlePost = () => {
+		ApiCall.post('/api', { name: 'postName' })
+			.then((Response) => {
+				setData(Response.data.message);
+				console.log('POST Method is success');
+				console.log(data);
+			})
+			.catch((error) => {
+				console.log('error :>> ', error);
+			});
+	};
+	return (
+		<div className="App">
+			<h1>{data ? data : null}</h1>
+			<button onClick={handleGet}>GET</button>
+			<button onClick={handlePost}>POST</button>
+		</div>
+	);
 }
 
 export default App;
