81bfe58223b0fb1cfb9710f1faa805c60787b7b1
ilyas kocak <kocakilyas18@gmail.com>
2021-04-16 13:00:35 +0300

    Product listed<

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import { useState } from 'react';
 import API from 'apisauce';
 import './App.css';
+import Product from './Product';
 
 const baseURL = 'https://private-e76dd9-kocakilyas.apiary-mock.com';
 const ApiCall = API.create({ baseURL: baseURL });
 function App() {
 	const [ data, setData ] = useState(null);
+	const [ products, setProducts ] = useState([
+		{
+			id: 1,
+			name: 'product1',
+			imageURL:
+				'https://images.pexels.com/photos/1667071/pexels-photo-1667071.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+			like: 301,
+			sellerPhotoURL: 'https://www.sellerphoto.com/wp-content/uploads/2016/11/JZ.png',
+			explanation: 'This product is awesome1!',
+			date: 'Mon Apr 12 2021 21:01:28 GMT+0300 (GMT+03:00)',
+			price: 101
+		}
+	]);
 
 	const handleGet = () => {
 		ApiCall.get('/api')
-			.then((Response) => {
-				setData(Response.data.message);
+			.then((res) => {
+				setData(res.data.message);
+				setProducts(res.data.products);
 				console.log('GET Method is success');
-				console.log(data);
 			})
 			.catch((error) => {
 				console.log('error :>> ', error);
@@ -20,8 +34,8 @@ function App() {
 	};
 	const handlePost = () => {
 		ApiCall.post('/api', { name: 'postName' })
-			.then((Response) => {
-				setData(Response.data.message);
+			.then((res) => {
+				setData(res.data.message);
 				console.log('POST Method is success');
 				console.log(data);
 			})
@@ -33,9 +47,9 @@ function App() {
 		<div className="App">
 			<h1>{data ? data : null}</h1>
 			<button onClick={handleGet}>GET</button>
+			<div>{products.map((product) => <Product product={product} />)}</div>
 			<button onClick={handlePost}>POST</button>
 		</div>
 	);
 }
-
 export default App;
---
+++ src/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Product = (product) => {
+    console.log('product :>> ', product);
+    return (
+        <div>
+            <h1>id</h1>
+        </div>
+    )
+}
+
+export default Product
