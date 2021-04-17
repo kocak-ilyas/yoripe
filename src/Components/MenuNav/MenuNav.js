31f26a74f114f130a4fef75b6e0caa89f9584773
ilyas kocak <kocakilyas18@gmail.com>
2021-04-17 23:48:04 +0300

    MenuNav done

--- src/Components/MenuNav/MenuNav.js
+++ src/Components/MenuNav/MenuNav.js
@@ -1,78 +1,11 @@
 import React, { useState } from 'react';
 import { Collapse } from 'reactstrap';
-import menuIcon from '../../image/menuIcon.svg';
-import './styles.css';
-//
-import { fade, makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
+import IconButton from '@material-ui/core/IconButton';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 
-const useStyles = makeStyles((theme) => ({
-	grow: {
-		flexGrow: 1
-	},
-	menuButton: {
-		marginRight: theme.spacing(2)
-	},
-	title: {
-		display: 'none',
-		[theme.breakpoints.up('sm')]: {
-			display: 'block'
-			//   display: 'none',
-		}
-	},
-	search: {
-		position: 'relative',
-		borderRadius: theme.shape.borderRadius,
-		backgroundColor: fade(theme.palette.common.white, 0.15),
-		'&:hover': {
-			backgroundColor: fade(theme.palette.common.white, 0.25)
-		},
-		marginRight: theme.spacing(2),
-		marginLeft: 0,
-		width: '100%',
-		[theme.breakpoints.up('sm')]: {
-			marginLeft: theme.spacing(3),
-			width: 'auto'
-		}
-	},
-	searchIcon: {
-		padding: theme.spacing(0, 2),
-		height: '100%',
-		position: 'absolute',
-		pointerEvents: 'none',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	inputRoot: {
-		color: 'inherit'
-	},
-	inputInput: {
-		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-		transition: theme.transitions.create('width'),
-		width: '100%',
-		[theme.breakpoints.up('md')]: {
-			width: '20ch'
-		}
-	},
-	sectionDesktop: {
-		display: 'flex',
-		[theme.breakpoints.up('md')]: {
-			display: 'flex'
-		}
-	},
-	sectionMobile: {
-		display: 'none',
-		[theme.breakpoints.up('md')]: {
-			display: 'none'
-		}
-	}
-}));
-//
+import menuIcon from '../../image/menuIcon.svg';
+import './styles.css';
 
 const MenuNav = () => {
 	const [ isOpen, setIsOpen ] = useState(false);
@@ -152,5 +85,4 @@ const MenuNav = () => {
 		</div>
 	);
 };
-
 export default MenuNav;
